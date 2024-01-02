4cde109e8e35d65880551e887b8762b3263495f0
Dolraith <Dolraith@Gmail.com>
2024-01-01 19:40:30 -0500

    nav items

--- routes.js
+++ routes.js
@@ -5,3 +5,6 @@ global._router.setRoutes(['svagrack'], 'modules/BoardNOrderSVG/_controllers/c_in
 global._router.setRoutes(['svagrack/index'], 'modules/BoardNOrderSVG/_controllers/c_index');
 global._router.setRoutes(['svagrack/folders'], 'modules/BoardNOrderSVG/_controllers/c_folders');
 global._router.setRoutes(['svagrack/tags'], 'modules/BoardNOrderSVG/_controllers/c_tags');
+
+
+global._router.setNav("SvagRack", "/svagrack", "Browse SVGs", null);
